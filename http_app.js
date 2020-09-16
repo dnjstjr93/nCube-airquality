82c1917fd6606f6bf93355e17c6ed6cd2eb05a33
dnjstjr93 <dnjstjr93@gmail.com>
2020-09-16 14:00:16 +0900

    - edit data_interval

--- http_app.js
+++ http_app.js
@@ -36,8 +36,8 @@ global.air_mission_name = '';
 global.my_sortie_name = 'disarm';
 
 const normal_interval = 100;
-const retry_interval = 1000;
-var data_interval = 10000;
+const retry_interval = 2500;
+var data_interval = 1000;
 
 var app = express();
 
