e731f8f820a9c368110ce75be6713d95d2af0881
dnjstjr93 <dnjstjr93@gmail.com>
2020-09-16 11:38:14 +0900

    - remove while, sleep

--- sensirion-sps030/test.py
+++ sensirion-sps030/test.py
@@ -14,44 +14,45 @@ def test(sensor_port, console_log_level):
 
     #SPS030.logger.info("reading: %s",mess)
     result = str(mess).split(", ")
-    print ("Mass Concentration PM1.0: ", result[1], "ug/m^3")
-    print ("Mass Concentration PM2.5: ", result[2], "ug/m^3")
-    print ("Mass Concentration PM4.0: ", result[3], "ug/m^3")
-    print ("Mass Concentration PM10: ", result[4], "ug/m^3")
-    print ("Number Concentration PM0.5: ", result[5], "#/cm^3")
-    print ("Number Concentration PM1.0: ", result[6], "#/cm^3")
-    print ("Number Concentration PM2.5: ", result[7], "#/cm^3")
-    print ("Number Concentration PM4.0: ", result[8], "#/cm^3")
-    print ("Number Concentration PM10: ", result[9], "#/cm^3")
-    print ("Typical Particle Size: ", result[10], "um\n")
+    # print ("Mass Concentration PM1.0: ", result[1], "ug/m^3")
+    # print ("Mass Concentration PM2.5: ", result[2], "ug/m^3")
+    # print ("Mass Concentration PM4.0: ", result[3], "ug/m^3")
+    # print ("Mass Concentration PM10: ", result[4], "ug/m^3")
+    # print ("Number Concentration PM0.5: ", result[5], "#/cm^3")
+    # print ("Number Concentration PM1.0: ", result[6], "#/cm^3")
+    # print ("Number Concentration PM2.5: ", result[7], "#/cm^3")
+    # print ("Number Concentration PM4.0: ", result[8], "#/cm^3")
+    # print ("Number Concentration PM10: ", result[9], "#/cm^3")
+    # print ("Typical Particle Size: ", result[10], "um\n")
+
+    return result
 
 
+# if __name__ == "__main__":
+#     ARG_PARSER = ArgumentParser(
+#         description="Return one reading from the SPS030 sensor attached to the speficied serial port")
+#     LOGGING_OUTPUT = ARG_PARSER.add_mutually_exclusive_group()
+#     LOGGING_OUTPUT.add_argument(
+#         "-q",
+#         "--quiet",
+#         action="store_true",
+#         help="Suppress most ouput")
+#     LOGGING_OUTPUT.add_argument(
+#          "-v",
+#          "--verbose",
+#          action="store_true",
+#          help="Maximum verbosity output on command line")
+# #     ARG_PARSER.add_argument(
+# #        "sensor_port",
+# #         action="store",
+# #         help="Port to which the sensor is connected")
+#     ARGS = ARG_PARSER.parse_args()
+#     CONSOLE_LOG_LEVEL = logging.INFO
+#     if ARGS.quiet:
+#         CONSOLE_LOG_LEVEL = logging.ERROR
+#     elif ARGS.verbose:
+#         CONSOLE_LOG_LEVEL = logging.DEBUG
 
-if __name__ == "__main__":
-    ARG_PARSER = ArgumentParser(
-        description="Return one reading from the SPS030 sensor attached to the speficied serial port")
-    LOGGING_OUTPUT = ARG_PARSER.add_mutually_exclusive_group()
-    LOGGING_OUTPUT.add_argument(
-        "-q",
-        "--quiet",
-        action="store_true",
-        help="Suppress most ouput")
-    LOGGING_OUTPUT.add_argument(
-         "-v",
-         "--verbose",
-         action="store_true",
-         help="Maximum verbosity output on command line")
-#     ARG_PARSER.add_argument(
-#        "sensor_port",
-#         action="store",
-#         help="Port to which the sensor is connected")
-    ARGS = ARG_PARSER.parse_args()
-    CONSOLE_LOG_LEVEL = logging.INFO
-    if ARGS.quiet:
-        CONSOLE_LOG_LEVEL = logging.ERROR
-    elif ARGS.verbose:
-        CONSOLE_LOG_LEVEL = logging.DEBUG
-
-    while True:
-        # test(ARGS.sensor_port, CONSOLE_LOG_LEVEL)
-        test('/dev/ttyUSB3', CONSOLE_LOG_LEVEL)
+#     while True:
+#         # test(ARGS.sensor_port, CONSOLE_LOG_LEVEL)
+#         test('/dev/ttyUSB3', CONSOLE_LOG_LEVEL)
