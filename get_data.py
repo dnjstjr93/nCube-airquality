607ea2dc627efe105c608207c7a20ceee9d5cec7
dnjstjr93 <dnjstjr93@gmail.com>
2020-09-16 13:33:51 +0900

    - parsing

--- get_data.py
+++ get_data.py
@@ -77,13 +77,13 @@ def json_to_val(json_val):
 
 def val_to_json(val):
     print(type(val))
-    if (type(val) == 'INT'):
+    if (str(type(val)) == "<class 'int>"):
         json_val = {"val":val}
         json_val = json.dumps(json_val)
         # json_val = {"val":val,"val2":val2}
-    # else:
-    #     json_val = {"val":val}
-    #     json_val = json.dumps(json_val)
+    else:
+        json_val = {"val":val[0],"val2":val[1],"val3":val[2],"val4":val[3],"val5":val[4],"val6":val[5],"val7":val[6],"val8":val[7],"val9":val[8],"val10":val[9]}
+        json_val = json.dumps(json_val)
 
     return (json_val)
 #-----------------------------------------------------------------------
