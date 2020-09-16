444a0e1cf168b3cbf5a29ab3ebbfdd6c8ffc9e2c
dnjstjr93 <dnjstjr93@gmail.com>
2020-09-16 11:41:43 +0900

    - edit lib folder name

--- get_data.py
+++ get_data.py
@@ -1,10 +1,10 @@
 import sys, os, json
 import time, board, busio
 import paho.mqtt.client as mqtt
-from CO import ze07
-from co2 import co2
+from lib import ze07
+from lib import co2
 import adafruit_ccs811
-from sensirion_sps030 import sensirion_sps030
+from lib import sensirion_sps030
 from argparse import ArgumentParser
 import logging
 
