562a2971f17f225dc0349f431eab30f5c54892ca
Wonseok Jung <54163029+dnjstjr93@users.noreply.github.com>
2020-10-15 23:57:09 +0900

    - add print log

--- sensor_test/CO/ze07.py
+++ sensor_test/CO/ze07.py
@@ -4,6 +4,7 @@ import serial
 class Ze07UartReader:
     def __init__(self):
         self.m_serial = serial.Serial("/dev/ttyUSB5", 9600)
+        print ("connect serial port ttyUSB5")
         self.m_stack = []
         
         
@@ -59,11 +60,11 @@ class Ze07UartReader:
                 #continue
                 pass
                 
-            #print('high:{}, low:{}'.format(self.m_stack[4], self.m_stack[5]))
+            print('high:{}, low:{}'.format(self.m_stack[4], self.m_stack[5]))
             high = int(self.m_stack[4].hex(), 16)
             low = int(self.m_stack[5].hex(), 16)
             ppm = (high*256 + low) * 0.1
-            # print('integer: {} ppm'.format(ppm))
+            print('integer: {} ppm'.format(ppm))
                 
             self.m_stack = []
             
